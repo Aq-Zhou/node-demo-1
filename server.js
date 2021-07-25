0ce7162a9d365396693cf4916fd6bfd76927a06f
Aq-Zhou <87415838+Aq-Zhou@users.noreply.github.com>
2021-07-25 19:58:12 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var http = require('http')
+ar http = require('http')
 var fs = require('fs')
 var url = require('url')
 var port = process.argv[2]
@@ -20,27 +20,35 @@ var server = http.createServer(function(request, response){
   /******** 从这里开始看，上面不要看 ************/
 
   console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
+
   if(path === '/'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.write(`
-    <!DOCTYPE html>
-  <head>
-  <link rel="stylesheet" href="/x"
-  </head>
-  <body>
-  </body>
+          <!DOCTYPE html>
+          <head>
+          <link rel="stylesheet" href="/x">
+          </head>
+          <body>
+          <h1>二哈</h1>
+          <script src="/y"></script>
+          </body>
     `)
     response.end()
-  } else if(path === '/style'){
+  } else if(path === '/x'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/css;charset=utf-8')
-    response.write(`h1{color: red;}`)
+    response.write(`body{color: red;}`)
+    response.end()
+  } else if(path === '/y'){
+    response.statusCode = 200
+    response.setHeader('Content-Type','text/javascript;charset=utf-8')
+    response.write(`这是js内容`)
     response.end()
   } else {
     response.statusCode = 404
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`你输入的路径不存在对应的内容`)
+    response.write(`你访问的页面不存在`)
     response.end()
   }
 
